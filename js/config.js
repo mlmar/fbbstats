bb232171ecb003c6fda1ecd74a9925dfba293596
mlmar <marcloussm@gmail.com>
2022-10-12 22:12:57 -0700

    Config update

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   DATA_PATH: 'https://raw.githubusercontent.com/mlmar/fbbstats/master/data/players-2021.json',
-  CATEGORIES: ['FG%', 'FT%', 'TGM', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'TOV'],
+  CATEGORIES: ['FG%', 'FT%', '3P', 'PTS', 'TRB', 'AST', 'STL', 'BLK', 'TOV'],
   CATEGORIES_LABELS: ['FG%', 'FT%', '3PTM', 'PTS', 'REB', 'AST', 'ST', 'BLK', 'TO'],
   MAX_PLAYERS: 150
 }
