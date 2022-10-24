2e46cc57b1c62d4d754ad05fc58df7af5adaeaea
marcus <marcloussm@gmail.com>
2022-10-24 00:39:54 -0700

    Changed min games and max players limits

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@ const CONFIG = {
   DATA_PATH: 'https://raw.githubusercontent.com/mlmar/fbbstats/master/data/players-2021.json',
   CATEGORIES: ['FG%', 'FT%', '3P', 'PTS', 'TRB', 'AST', 'STL', 'BLK', 'TOV'],
   CATEGORIES_LABELS: ['FG%', 'FT%', '3PTM', 'PTS', 'REB', 'AST', 'ST', 'BLK', 'TO'],
-  MAX_PLAYERS: 150,
-  MIN_GAMES: 30,
+  MAX_PLAYERS: 300,
+  MIN_GAMES: 0,
   USER: 'USER'
 }
 
